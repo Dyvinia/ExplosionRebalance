ec28936b948d1aa42560f6c9de090bd9cb55ad00
Dyvinia <dulana57@gmail.com>
2025-10-20 16:04:05 -0400

    Initial commit

---
+++ common/src/main/java/org/dyvinia/explosionrebalance/Constants.java
@@ -0,0 +1,11 @@
+package org.dyvinia.explosionrebalance;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "explosionrebalance";
+    public static final String MOD_NAME = "ExplosionRebalance";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
